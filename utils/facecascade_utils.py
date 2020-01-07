1a28d6545c7f8d461dfff118e6318af3784c73c2
Bartosz <bartptak@gmail.com>
2020-01-07 10:25:27 +0100

    Add scale to facecascade

--- utils/facecascade_utils.py
+++ utils/facecascade_utils.py
@@ -9,10 +9,20 @@ class HaarModel:
                      'models', 'haarcascade', 'haarcascade_frontalface_default.xml')
                  ):
         self.face_cascade = cv2.CascadeClassifier(xml_path)
+        self.size = size
 
     def predict(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+        scale = img.shape[0]/self.size
+
+        gray = cv2.resize(gray, (int(img.shape[1]*scale), int(img.shape[0]*scale)))
+
         faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
 
-        return faces
+        faces_new = []
+        for ff in faces:
+            faces_new.append([int(f/scale) for f in ff])
+
+        print(faces_new)
+        return faces_new
